f30ef0b13e6e8c7e7f162c415c8a580baa7e259a
Josh <evahybrid@gmail.com>
2012-05-19 15:13:05 -0400

    Fixing user name

---
+++ app/models/User.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Josh
+ * Date: 5/19/12
+ * Time: 3:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+}
---
+++ test/UserTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Josh
+ * Date: 5/19/12
+ * Time: 3:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserTest {
+}
